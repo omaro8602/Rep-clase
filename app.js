5da76175d0b00118cc4814c26bd1e5942a0a46a3
omaro8602 <111027802+omaro8602@users.noreply.github.com>
2022-10-07 16:07:42 -0500

    version1

---
+++ app.js
@@ -0,0 +1,4 @@
+// let num1 = parseInt (prompt("ingrese el numero 1"))
+// let num2 = parseInt (prompt("ingrese el numero 2"))
+// let suma= num1+num2
+// alert ("la suma de los dos numeros es:" + suma)
